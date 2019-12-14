659fb04be5eb82e2dc1fa5e822d5eab1317b80af
Mingtao Li <mingtao@xjtu.edu.cn>
2019-12-14 09:24:06 +0800

    updated ircre.bib

---
+++ ircreupdate/robotcheck.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/limingtao/ircreupdate/scholarpy/scholar-gui.py -c 1 -t --phrase "CdS nanocrystallites sensitized ZnO nanorods with plasmon enhanced photoelectrochemical performance"
